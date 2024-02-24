6a359a9808bce21aced1438df4074a340b5bfb90
palmibb <cpalmi0725@gmail.com>
2024-02-24 14:20:42 -0500

    updated next js config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
 // next.config.ts
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "cdn.sanity.io",
+//         port: "",
+//       },
+//     ],
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = {
   images: {
+    domains: ['cdn.sanity.io', 'localhost'], // Add 'localhost' as a domain for local images
     remotePatterns: [
       {
         protocol: "https",
