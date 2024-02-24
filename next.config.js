fa5e3104dd4f536709b97320f3066fe25a714c82
palmibb <cpalmi0725@gmail.com>
2024-02-24 14:29:18 -0500

    reverted changes to nextconfigjs

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 // next.config.ts
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// module.exports = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "cdn.sanity.io",
-//         port: "",
-//       },
-//     ],
-//   },
-// };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = {
   images: {
-    domains: ['cdn.sanity.io', 'localhost'], // Add 'localhost' as a domain for local images
     remotePatterns: [
       {
         protocol: "https",
